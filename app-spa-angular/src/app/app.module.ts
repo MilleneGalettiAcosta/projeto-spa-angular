483bf46b693770cc1435eae791627458abd2d63c
Millene Galetti <mikagaletti1@gmail.com>
2022-07-26 09:52:38 -0300

    Feat: Adiciona componente navbar

--- app-spa-angular/src/app/app.module.ts
+++ app-spa-angular/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
